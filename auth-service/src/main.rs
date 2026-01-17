e06c3da9e415c7654680d06edcf4fbe165e26a72
Sachin Bhat <sachubhat17@gmail.com>
2026-01-17 16:29:57 +0800

    refactor(auth-service): [Sprint 1] [Task 3]  move app setup into lib and simplify main

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,45 +1,13 @@
-use axum::{Router, extract::Query, response::Html, routing::get, serve};
-use serde::Deserialize;
-use tower_http::services::ServeDir;
+use auth_service::Application;
 
 #[tokio::main]
 async fn main() {
-    let assets_dir = ServeDir::new("assets");
-    let app = Router::new()
-        .fallback_service(assets_dir)
-        .route("/hello", get(hello_handler));
-
     // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
     // This is needed for Docker to work, which we will add later on.
     // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
+    let app = Application::build("0.0.0.0:3000")
+        .await
+        .expect("Failed to build application");
 
-    axum::serve(listener, app).await.unwrap();
-}
-
-#[derive(Deserialize)]
-struct HelloQuery {
-    name: Option<String>,
-}
-
-async fn hello_handler(Query(query): Query<HelloQuery>) -> Html<String> {
-    let name = query.name.as_deref().unwrap_or("friend");
-    let name = escape_html(name);
-    Html(format!("<h1>Hello, {}!</h1>", name))
-}
-
-fn escape_html(input: &str) -> String {
-    let mut escaped = String::with_capacity(input.len());
-    for ch in input.chars() {
-        match ch {
-            '&' => escaped.push_str("&amp;"),
-            '<' => escaped.push_str("&lt;"),
-            '>' => escaped.push_str("&gt;"),
-            '"' => escaped.push_str("&quot;"),
-            '\'' => escaped.push_str("&#39;"),
-            _ => escaped.push(ch),
-        }
-    }
-    escaped
+    app.run().await.expect("Failed to run application");
 }
