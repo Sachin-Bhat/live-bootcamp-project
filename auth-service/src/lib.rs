c11eee0c83afef98e9e9706c1164fde1e5bbcc78
Sachin Bhat <sachubhat17@gmail.com>
2026-01-20 23:18:09 +0800

    feat(auth-service): [Sprint 1] [Task 4] add auth routes and stub handlers
    Signed-off-by: Sachin Bhat <sachubhat17@gmail.com>

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
 
-use axum::serve::Serve;
 use axum::Router;
+use axum::{response::IntoResponse, routing::post, serve::Serve};
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
@@ -18,9 +18,15 @@ impl Application {
         // Move the Router definition from `main.rs` to here.
         // Also, remove the `hello` route.
         // We don't need it at this point!
-        let assets_dir = ServeDir::new("assets");
-        let router = Router::new().fallback_service(assets_dir);
-
+        let assets_dir =
+            ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+        let router = Router::new()
+            .fallback_service(assets_dir)
+            .route("/signup", post(signup))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token))
+            .route("/login", post(login))
+            .route("/logout", post(logout));
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
@@ -33,4 +39,27 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+// Example route handler.
+// For now we will simply return a 201 (CREATED) status code.
+async fn signup() -> impl IntoResponse {
+    reqwest::StatusCode::CREATED.into_response()
+}
+
+// TODO: Add all other route handlers with appropriate status codes
+async fn login() -> impl IntoResponse {
+    reqwest::StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    reqwest::StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    reqwest::StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    reqwest::StatusCode::OK.into_response()
 }
