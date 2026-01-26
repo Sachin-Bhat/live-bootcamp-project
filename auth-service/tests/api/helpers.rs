532d2e7cf6116596a3de07c6e5d4fed64a27654b
Sachin Bhat <sachubhat17@gmail.com>
2026-01-26 11:23:34 +0800

    refactor(auth-service): [Sprint 2] [Task 3] use default HashmapUserStore initialization
    Signed-off-by: Sachin Bhat <sachubhat17@gmail.com>

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -11,7 +11,7 @@ pub struct TestApp {
 
 impl TestApp {
     pub async fn new() -> Self {
-        let user_store = HashmapUserStore::new();
+        let user_store = HashmapUserStore::default();
         let app_state = AppState::new(Arc::new(RwLock::new(user_store)));
 
         let app = Application::build(app_state, "127.0.0.1:0")
