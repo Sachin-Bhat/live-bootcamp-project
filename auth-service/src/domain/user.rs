26c841abcb2f0e21211236317958c1a9e6dcadfb
Sachin Bhat <sachubhat17@gmail.com>
2026-01-26 12:48:33 +0800

    refactor(auth-service): [Sprint 2] [Task 4] use Email/Password in User and signup flow
    Signed-off-by: Sachin Bhat <sachubhat17@gmail.com>

--- auth-service/src/domain/user.rs
+++ auth-service/src/domain/user.rs
@@ -1,18 +1,19 @@
 // The User struct should contain 3 fields. email, which is a String;
 // password, which is also a String; and requires_2fa, which is a boolean.
 
+use crate::domain::{Email, Password};
 use serde::Serialize;
 
 #[derive(Serialize, Clone)]
 pub struct User {
-    pub(crate) email: String,
-    pub(crate) password: String,
+    pub(crate) email: Email,
+    pub(crate) password: Password,
     #[serde(rename = "requires2FA")]
     requires_2fa: bool,
 }
 
 impl User {
-    pub fn new(email: String, password: String, requires_2fa: bool) -> Self {
+    pub fn new(email: Email, password: Password, requires_2fa: bool) -> Self {
         User {
             email,
             password,
