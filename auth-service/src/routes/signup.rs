d9f1e7eba99a53ddeba88ad73035c767d58d4b5d
Sachin Bhat <sachubhat17@gmail.com>
2026-01-26 10:44:33 +0800

    feat(auth-service): [Sprint 2] [Task 2] add AuthAPIError and use it in signup responses
    Signed-off-by: Sachin Bhat <sachubhat17@gmail.com>

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,28 +1,51 @@
 use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
 use serde::{Deserialize, Serialize};
 
-use crate::{app_state::AppState, domain::User};
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, User},
+};
 
 pub async fn signup(
     State(state): State<AppState>,
     Json(request): Json<SignupRequest>,
-) -> impl IntoResponse {
-    // Create a new `User` instance using data in the `request`
-    let user = User::new(request.email, request.password, request.requires_2fa);
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let SignupRequest {
+        email,
+        password,
+        requires_2fa,
+    } = request;
+
+    // early return AuthAPIError::InvalidCredentials if:
+    // - email is empty or does not contain '@'
+    // - password is less than 8 characters
+    if email.is_empty() || !email.contains('@') || password.len() < 8 {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
 
     let mut user_store = state.user_store.write().await;
 
-    // TODO: Add `user` to the `user_store`. Simply unwrap the returned `Result` enum type for now.
-    user_store.add_user(user).unwrap();
+    // early return AuthAPIError::UserAlreadyExists if email exists in user_store.
+    if user_store.get_user(&email).is_ok() {
+        return Err(AuthAPIError::UserAlreadyExists);
+    }
+
+    // Create a new `User` instance using data in the `request`
+    let user = User::new(email, password, requires_2fa);
+
+    // instead of using unwrap, early return AuthAPIError::UnexpectedError if add_user() fails.
+    user_store
+        .add_user(user)
+        .map_err(|_| AuthAPIError::UnexpectedError)?;
 
     let response = Json(SignupResponse {
         message: "User created successfully!".to_string(),
     });
 
-    (StatusCode::CREATED, response)
+    Ok((StatusCode::CREATED, response))
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct SignupRequest {
     pub email: String,
     pub password: String,
