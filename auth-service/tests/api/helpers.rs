874bd3a3d8c5f8a4a7518ab8a46989e93287e272
Sachin Bhat <sachubhat17@gmail.com>
2026-03-12 11:13:28 +0800

    feat(auth): wire email client into app state and 2fa login flow
    Signed-off-by: Sachin Bhat <sachubhat17@gmail.com>

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -2,10 +2,12 @@ use std::sync::Arc;
 
 use auth_service::{
     Application,
-    app_state::{AppState, BannedTokenStoreType, TwoFACodeStoreType},
+    app_state::{
+        AppState, BannedTokenStoreType, EmailClientType, TwoFACodeStoreType, UserStoreType,
+    },
     services::{
         hashmap_two_fa_code_store::HashmapTwoFACodeStore, hashmap_user_store::HashmapUserStore,
-        hashset_banned_token_store::HashsetBannedTokenStore,
+        hashset_banned_token_store::HashsetBannedTokenStore, mock_email_client::MockEmailClient,
     },
     utils::constants::test,
 };
@@ -15,23 +17,28 @@ use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
+    pub user_store: UserStoreType,
     pub banned_token_store: BannedTokenStoreType,
     pub two_fa_code_store: TwoFACodeStoreType,
+    pub email_client: EmailClientType,
     pub cookie_jar: Arc<Jar>,
     pub http_client: reqwest::Client,
 }
 
 impl TestApp {
     pub async fn new() -> Self {
-        let user_store = Arc::new(RwLock::new(HashmapUserStore::default()));
+        let user_store: UserStoreType =
+            Arc::new(RwLock::new(Box::new(HashmapUserStore::default())));
         let banned_token_store: BannedTokenStoreType =
             Arc::new(RwLock::new(Box::new(HashsetBannedTokenStore::default())));
         let two_fa_code_store: TwoFACodeStoreType =
             Arc::new(RwLock::new(Box::new(HashmapTwoFACodeStore::default())));
+        let email_client: EmailClientType = Arc::new(RwLock::new(Box::new(MockEmailClient)));
         let app_state = AppState::new(
-            user_store,
+            user_store.clone(),
             banned_token_store.clone(),
             two_fa_code_store.clone(),
+            email_client.clone(),
         );
 
         let app = Application::build(app_state, test::APP_ADDRESS)
@@ -54,8 +61,10 @@ impl TestApp {
         // Create new `TestApp` instance and return it
         Self {
             address,
+            user_store,
             banned_token_store,
             two_fa_code_store,
+            email_client,
             cookie_jar,
             http_client,
         }
